72cb214322af862bc47b54227f7ff7f552f4691b
Willem Allan <willemarf@gmail.com>
2022-02-11 16:12:49 -0300

    add dir django

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python django/manage.py makemigrations
+python django/manage.py migrate
+python django/manage.py runserver 0.0.0.0:8000
